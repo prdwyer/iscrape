2f1774b794db11e76ad37ba7d34c4cd565b90693
test <test>
2020-01-05 13:46:21 -0500

    initial

---
+++ iscrape.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import argparse
+import subprocess
+from ioc_finder import find_iocs
+
+#dependencies pdfminer and ioc-finder
+
+
+if __name__ == '__main__':
+
+
+	parser = argparse.ArgumentParser(description='iocs from documents')
+	parser.add_argument("-f","--file", dest="doc", default=None, help="file to search")
+	args = parser.parse_args()
+	cmd = "pdf2txt.py " +args.doc	
+
+	text = subprocess.getoutput(cmd)
+	iocs = find_iocs(text)
+	
+	report = ""
+	report = args.doc + "\n\n"
+
+	for ioc in iocs:
+		output = ""
+		for thing in iocs[ioc]:
+			output += thing+"\n"
+			#print(thing)
+
+		if output:
+			output = ioc +":\n"+ output
+			report += output+"\n"
+	
+	print(report)
+
