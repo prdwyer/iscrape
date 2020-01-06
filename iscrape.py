c5b28cbca8f0cc26b4fae2c5b3ed8c22b122fc3e
Dwyer <prdwyer87@gmail.com>
2020-01-06 11:11:45 -0500

    added doc

--- iscrape.py
+++ iscrape.py
@@ -5,13 +5,25 @@ import os.path
 import subprocess
 from ioc_finder import find_iocs
 
-#dependencies pdfminer and ioc-finder
+#dependencies pdfminer and ioc-finder and textutil
 
+def scan_and_report(text):
 
+	iocs = find_iocs(text)
+	report = ""
+	report = args.doc + "\n\n"
+	for ioc in iocs:
+		output = ""
+		for thing in iocs[ioc]:
+			output += thing+"\n"
+			#print(thing)
+		if output:
+			output = ioc +":\n"+ output
+			report += output+"\n"
+	return report
 
 
 if __name__ == '__main__':
-
 
 	parser = argparse.ArgumentParser(description='iocs from documents')
 	parser.add_argument("-f","--file", dest="doc", default=None, help="file to search")
@@ -22,41 +34,13 @@ if __name__ == '__main__':
 	if "pdf" in extension:
 		cmd = "pdf2txt.py " +args.doc	
 		text = subprocess.getoutput(cmd)
-		iocs = find_iocs(text)
-	
-		report = ""
-		report = args.doc + "\n\n"
-
-		for ioc in iocs:
-			output = ""
-			for thing in iocs[ioc]:
-				output += thing+"\n"
-				#print(thing)
-
-			if output:
-				output = ioc +":\n"+ output
-				report += output+"\n"
-		
-		print(report)
+		print(scan_and_report(text))
 
 	elif ("doc" or "ppt")in extension:
-		print("doc file")	
+		cmd = "textutil -convert txt -stdout " +args.doc
+		text = subprocess.getoutput(cmd)
+		print(scan_and_report(text))
 	else:
 		cmd = "cat " +args.doc	
 		text = subprocess.getoutput(cmd)
-		iocs = find_iocs(text)
-	
-		report = ""
-		report = args.doc + "\n\n"
-
-		for ioc in iocs:
-			output = ""
-			for thing in iocs[ioc]:
-				output += thing+"\n"
-				#print(thing)
-
-			if output:
-				output = ioc +":\n"+ output
-				report += output+"\n"
-		
-		print(report)
+		print(scan_and_report(text))
