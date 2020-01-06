bb096198c1b0d4a03e5ab2caa3a9d35186a912cc
test <test>
2020-01-06 09:13:33 -0500

    revise

--- iscrape.py
+++ iscrape.py
@@ -1,10 +1,13 @@
 import os
 import sys
 import argparse
+import os.path
 import subprocess
 from ioc_finder import find_iocs
 
 #dependencies pdfminer and ioc-finder
+
+
 
 
 if __name__ == '__main__':
@@ -13,23 +16,47 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='iocs from documents')
 	parser.add_argument("-f","--file", dest="doc", default=None, help="file to search")
 	args = parser.parse_args()
-	cmd = "pdf2txt.py " +args.doc	
+	filename = args.doc
+	extension = os.path.splitext(filename)[-1]
 
-	text = subprocess.getoutput(cmd)
-	iocs = find_iocs(text)
+	if "pdf" in extension:
+		cmd = "pdf2txt.py " +args.doc	
+		text = subprocess.getoutput(cmd)
+		iocs = find_iocs(text)
 	
-	report = ""
-	report = args.doc + "\n\n"
+		report = ""
+		report = args.doc + "\n\n"
 
-	for ioc in iocs:
-		output = ""
-		for thing in iocs[ioc]:
-			output += thing+"\n"
-			#print(thing)
+		for ioc in iocs:
+			output = ""
+			for thing in iocs[ioc]:
+				output += thing+"\n"
+				#print(thing)
 
-		if output:
-			output = ioc +":\n"+ output
-			report += output+"\n"
+			if output:
+				output = ioc +":\n"+ output
+				report += output+"\n"
+		
+		print(report)
+
+	elif ("doc" or "ppt")in extension:
+		print("doc file")	
+	else:
+		cmd = "cat " +args.doc	
+		text = subprocess.getoutput(cmd)
+		iocs = find_iocs(text)
 	
-	print(report)
+		report = ""
+		report = args.doc + "\n\n"
 
+		for ioc in iocs:
+			output = ""
+			for thing in iocs[ioc]:
+				output += thing+"\n"
+				#print(thing)
+
+			if output:
+				output = ioc +":\n"+ output
+				report += output+"\n"
+		
+		print(report)
